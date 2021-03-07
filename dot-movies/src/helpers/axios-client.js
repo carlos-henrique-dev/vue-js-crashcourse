daa89efcf378153d0300432cffce537a935a7ff3
henrique <henriqueok20@gmail.com>
2021-03-06 21:47:11 -0400

    mudando url da api

--- dot-movies/src/helpers/axios-client.js
+++ dot-movies/src/helpers/axios-client.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-export const defaultUrl = "https://api.themoviedb.org/3";
-
 const axiosClient = axios.create({
-  baseURL: defaultUrl,
+  baseURL: process.env.VUE_APP_TMDB_API_URL,
   params: {
     language: "pt-BR",
     api_key: process.env.VUE_APP_TMDB_API_KEY,
