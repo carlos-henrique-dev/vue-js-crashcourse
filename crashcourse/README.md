8932838e962eca8be0dba21579623de24ed7afec
henrique <henriqueok20@gmail.com>
2021-03-26 17:39:08 -0400

    divide o repo

---
+++ crashcourse/README.md
@@ -0,0 +1,22 @@
+
+
+### vuex-todo-manager
+![Alt Text](/gifs/vuex-todo.gif)
+  - run `yarn` to install the dependencies
+  - enter the folder and run `yarn serve` to run the development server
+
+## vue-js-crashcourse
+
+this project were made following the [Traversy Media](https://www.youtube.com/watch?v=qZXt1Aom3Cs&ab_channel=TraversyMedia) VueJS crashcourse
+
+
+### vue-crash-2021
+  ![Alt Text](/gifs/simple-todo.gif)
+  - run `yarn` to install the dependencies
+  - enter de folder and run `yarn backend` to run the json-server 
+  - run `yarn serve` to run the development server
+
+### Random User Gen
+  ![Alt Text](/gifs/random-user.gif)
+  - Just open the `index.html` on the browser
+  
---
+++ crashcourse/vue-crash-course/src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <header>
+    <router-link to="/">
+      <h1>{{ title }}</h1>
+    </router-link>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+    },
+    showAddTask: {
+      type: Boolean,
+    },
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+a {
+  color: #000;
+  text-decoration: none;
+}
+</style>
